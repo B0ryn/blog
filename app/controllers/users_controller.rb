10a015f74905511e1594fb57da63e80165e54c14
Boryn <jakub.siarkiewicz1@gmail.com>
2022-03-22 23:29:00 +0100

    fix pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @list_of_users = User.all
+    @list_of_users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
